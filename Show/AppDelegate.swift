e8283a731bc431b82cea09c59e8d48eb05e32746
Fabian Canas <fcanas@gmail.com>
2015-01-08 17:07:40 -0500

    Initial commit

---
+++ Show/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Show
+//
+//  Created by Fabian Canas on 1/8/15.
+//  Copyright (c) 2015 Fabián Cañas. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ Show/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Show
+//
+//  Created by Fabian Canas on 1/8/15.
+//  Copyright (c) 2015 Fabián Cañas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
