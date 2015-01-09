e3df37508b8163c26ff546435f37156f7191f828
Fabian Canas <fcanas@gmail.com>
2015-01-09 17:21:00 -0500

    Connect to Parse

--- Show/AppDelegate.swift
+++ Show/AppDelegate.swift
@@ -14,7 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let keys = ShowKeys()
+        Parse.setApplicationId(keys.parseAppID(), clientKey: keys.parseClientKey())
+        
         return true
     }
 
